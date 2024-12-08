17d00e8a5201e9f6f2eaf1d12a4a7177756f780f
maheshsathe07 <sathem0712@gmail.com>
2024-12-08 18:11:09 +0530

    Conversion from TS to JS

--- app/api/helloNextJs/route.js
+++ app/api/helloNextJs/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET(request) {
   return NextResponse.json({ message: "Hello from Analyza!" });
 }
+
